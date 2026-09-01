bcffdc52d1eb09b523e8a46065e0203a3b16bd03
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2026-09-01 16:41:53 +0200

    Co-locate model type tests

---
+++ src/core/model.test-d.ts
@@ -0,0 +1,10 @@
+import { expectAssignable, expectNotAssignable } from "tsd";
+import type { EmberState, EvidenceId, MeaningId } from "./model.ts";
+
+declare const evidenceId: EvidenceId;
+declare const meaningId: MeaningId;
+declare const state: EmberState;
+
+expectAssignable<MeaningId>(meaningId);
+expectAssignable<EmberState>(state);
+expectNotAssignable<MeaningId>(evidenceId);
